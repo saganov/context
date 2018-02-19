b506ea631896ca20d08cd88dc51c26703b267e3a
Petro Saganov <petro.saganov@oracle.com>
2018-02-19 22:46:51 +0200

    [feature] add the validation of required context key
    Validate if the required key has been set

--- src/Context.php
+++ src/Context.php
@@ -14,10 +14,16 @@ class Context
     public function get($key){
         if ($this->scheme->contains($key)){
             $value = $this->driver->resolve($key, $this->scheme->default($key));
+            // TODO: there is knowing of Context about Scheme that
+            // default value NULL means required key
+            if (is_null($value)) throw new RequiredKeyException("Required context key: '{$key}' left unset");
             $value = $this->scheme->cast($key, $value);
             return $value;
         } else {
-            throw new Exception("Unknown context key: '{$key}' has requested");
+            throw new UnknownKeyException("Unknown context key: '{$key}' has requested");
         }
     }
 }
+
+class RequiredKeyException extends \Exception{}
+class UnknownKeyException extends \Exception{}
